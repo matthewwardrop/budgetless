871b4619fe8861d59038d50bbb28bd8ad81079c5
Matthew Wardrop <mister.wardrop@gmail.com>
2016-05-01 20:46:19 -0700

    Add support for per-day allocations. Definitely not optimal in performance, but for the number of allocations likely to be present; almost certainly good enough (TM).

--- budgetless/allocations.py
+++ budgetless/allocations.py
@@ -1,58 +1,97 @@
 from .db import DBPool, tbl_alloc
 from sqlalchemy import select, insert, delete
 import parampy
+import datetime
+
+# TODO: Consider optimising this module if required. In particular
+# the lookup by date leaves a lot to be desired.
 
 class AllocationPool(DBPool):
 
     def init(self):
         self.p = parampy.Parameters()
         self.p.scaling(time=(1, 'day'))
+        self.zero = self.p._( (0, '$/day') )
 
-    def get_expenses(self, date=None):
-        expenses = {}
-        for allocation in self.list():
-            if not allocation['income']:
-                expenses[allocation['category']] = self.p._( (allocation['amount'], '$/'+allocation['interval']) )
-        return expenses
+    def __get_contribution(self, allocation, date, error=False):
+        contrib = self.p._( (allocation['amount'], '$/'+allocation['interval']) )
 
-    def get_categories(self, date=None):
-        try:
-            return self.__categories
-        except:
-            self.__categories = list(set([d['category'] for d in self.list()]))
-            return self.__categories
+        if allocation['start'] is None and allocation['end'] is None:
+            return contrib
+
+        if date is None:
+            date = datetime.date.today()
+
+        included = True
+
+        if allocation['start'] is not None and allocation['start'] > date:
+            include = False
+
+        if allocation['end'] is not None and allocation['end'] < date:
+            include = False
+
+        if error and not include:
+            raise ValueError("Allocation does not contribute.")
+
+        return contrib if include else self.zero
 
     def get_income(self, date=None):
         income = {}
         for allocation in self.list():
             if allocation['income']:
-                income[allocation['category']] = self.p._( (allocation['amount'], '$/'+allocation['interval']) )
+                try:
+                    income[allocation['category']] = self.__get_contribution(allocation, date, error=True)
+                except ValueError:
+                    pass
         return income
+
+    def get_expenses(self, date=None):
+        expenses = {}
+        for allocation in self.list():
+            if not allocation['income']:
+                try:
+                    expenses[allocation['category']] = self.__get_contribution(allocation, date, error=True)
+                except ValueError:
+                    pass
+        return expenses
+
+    def get_categories(self, date=None):
+        categories = []
+        for allocation in self.list():
+            try:
+                self.__get_contribution(allocation, date, error=True)
+                categories.append(allocation['category'])
+            except ValueError:
+                pass
+        return categories
 
     def get_surplus(self, date=None):
         v = self.p._( (0,'$/day') )
-        print (self.get_income())
-        for income in self.get_income().values():
+        for income in self.get_income(date).values():
             v += income
-        for expense in self.get_expenses().values():
+        for expense in self.get_expenses(date).values():
             v -= expense
         return v
 
     def get_daily_surplus(self, date=None):
         return self.get_surplus(date)('$/day').value
 
-
     # Database functions
     def clear_cache(self):
         try:
+            del self.__list
             del self.__categories
         except:
             pass
 
     def list(self):
-        with self.engine.begin() as conn:
-            s = select([tbl_alloc])
-            return list(conn.execute(s))
+        try:
+            return self.__list
+        except:
+            with self.engine.begin() as conn:
+                s = select([tbl_alloc])
+                self.__list = list(conn.execute(s))
+                return self.__list
 
     def add(self, **opts):
         self.clear_cache()
